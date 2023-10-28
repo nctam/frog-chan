e3241429b4cc1689ac18f862347ec329bf0d7ccb
Tam Chi Nguyen <nctam109@gmail.com>
2023-10-28 18:30:27 +0700

    Add caching

--- logic/autoreact.go
+++ logic/autoreact.go
@@ -1,61 +1,61 @@
 package logic
 
 import (
-    "context"
-    discord "github.com/bwmarrin/discordgo"
-    "github.com/rs/zerolog"
-    "time"
+	"context"
+	discord "github.com/bwmarrin/discordgo"
+	"github.com/rs/zerolog"
+	"time"
 
-    "istio.io/pkg/cache"
-    "kaeru.chan/voz/constant"
-    internal "kaeru.chan/voz/message"
-    "kaeru.chan/voz/utils"
+	"istio.io/pkg/cache"
+	"kaeru.chan/voz/constant"
+	internal "kaeru.chan/voz/message"
+	"kaeru.chan/voz/utils"
 )
 
 const (
-    autoReactLogTag = "GeneralAutoReact"
-    cacheTTL        = 60
+	autoReactLogTag = "GeneralAutoReact"
+	cacheTTL        = 60
 )
 
 var (
-    autoReactCache cache.ExpiringCache
+	autoReactCache cache.ExpiringCache
 )
 
 type GeneralAutoReact struct{}
 
 func (g *GeneralAutoReact) SendReact(ctx context.Context, s *discord.Session, r *discord.MessageReactionAdd) {
-    log := zerolog.Ctx(ctx).With().Str(autoReactLogTag, "SendReact").Logger()
-    message, _ := s.ChannelMessage(r.ChannelID, r.MessageID)
-    msgAuthor := message.Author.ID
-    reactAuthor := r.Member.User.ID
-    if utils.StringContains(config.Bots, reactAuthor) && msgAuthor == constant.Bot {
-        log.Debug().Msgf("Reaction from %s found, removing", r.Member.User.Username)
-        if err := s.MessageReactionRemove(r.ChannelID, r.MessageID, r.Emoji.APIName(), reactAuthor); err != nil { // fucking naming emoji params
-            log.Err(err).Msgf("Unable to remove emoji %s from user %s", r.Emoji.ID, r.Member.User.Username)
-            return
-        }
+	log := zerolog.Ctx(ctx).With().Str(autoReactLogTag, "SendReact").Logger()
+	message, _ := s.ChannelMessage(r.ChannelID, r.MessageID)
+	msgAuthor := message.Author.ID
+	reactAuthor := r.Member.User.ID
+	if utils.StringContains(config.Bots, reactAuthor) && msgAuthor == constant.Bot {
+		log.Debug().Msgf("Reaction from %s found, removing", r.Member.User.Username)
+		if err := s.MessageReactionRemove(r.ChannelID, r.MessageID, r.Emoji.APIName(), reactAuthor); err != nil { // fucking naming emoji params
+			log.Err(err).Msgf("Unable to remove emoji %s from user %s", r.Emoji.ID, r.Member.User.Username)
+			return
+		}
 
-        value, _ := autoReactCache.Get(reactAuthor)
-        if value != nil && value.(int) > 0 {
-            log.Debug().Msgf("Reply reaction from %s has processed, continue in %d seconds", r.Member.User.Username, cacheTTL)
-            return
-        }
+		value, _ := autoReactCache.Get(reactAuthor)
+		if value != nil && value.(int) > 0 {
+			log.Debug().Msgf("Reply reaction from %s has processed, continue in %d seconds", r.Member.User.Username, cacheTTL)
+			return
+		}
 
-        msg := internal.Template{
-            Message:  "React cc nè",
-            Emoji:    constant.Tsk,
-            TagUsers: []string{config.Bots[0]},
-        }
+		msg := internal.Template{
+			Message:  "React cc nè",
+			Emoji:    constant.Tsk,
+			TagUsers: []string{config.Bots[0]},
+		}
 
-        time.Sleep(30 * time.Second)
-        _, err := s.ChannelMessageSend(r.ChannelID, msg.Build())
-        if err != nil {
-            log.Err(err).Msgf("Unable to send message %s to channel %s", msg.Message, r.ChannelID)
-        }
-        autoReactCache.Set(reactAuthor, 1)
-    }
+		time.Sleep(30 * time.Second)
+		_, err := s.ChannelMessageSend(r.ChannelID, msg.Build())
+		if err != nil {
+			log.Err(err).Msgf("Unable to send message %s to channel %s", msg.Message, r.ChannelID)
+		}
+		autoReactCache.Set(reactAuthor, 1)
+	}
 }
 
 func init() {
-    autoReactCache = cache.NewLRU(cacheTTL*time.Second, cacheTTL*time.Second, 500)
+	autoReactCache = cache.NewLRU(cacheTTL*time.Second, cacheTTL*time.Second, 500)
 }
