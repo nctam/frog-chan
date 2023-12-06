a435f2c9dfb98a095ecdb91a1b5d708db028ce73
im6h <danghaiv.18@gmail.com>
2023-12-06 22:20:09 +0700

    refactor: refactor function and update logic test.

--- utils/calculator_test.go
+++ utils/calculator_test.go
@@ -2,21 +2,12 @@ package utils
 
 import (
 	"fmt"
-	"golang.org/x/exp/rand"
 	"testing"
-	"time"
 )
 
-func TestShouldTarget(t *testing.T) {
-	t.Run("should target", func(t *testing.T) {
-		counter := 0
-		for i := 0; i < 100; i++ {
-			rand.Seed(uint64(time.Now().UnixNano()))
-			randValue := rand.Int()
-			if randValue%5 == 0 {
-				counter += 1
-			}
-		}
-		fmt.Println(counter)
+func TestMakeDecision(t *testing.T) {
+	t.Run("make decision", func(t *testing.T) {
+		counter, decision := makeDecision(0.2, 100)
+		fmt.Printf("Total votes: %d ==> decision: %v", counter, decision)
 	})
 }
