dfc1f48b5bfa7bf10b3d82d4171e7ad0934f1a96
Vu <35485655+im6h@users.noreply.github.com>
2025-02-06 23:00:14 +0700

    Update user.go (#12)

--- constant/user.go
+++ constant/user.go
@@ -9,6 +9,12 @@ const (
 
 const (
 	EchChanID = "666563738370834434"
+    CaDuChanID = "376386392311201793"
+    FonChanID = "367309546823221250"
+    PongChanID = "390488049420730369"
+    PiChanID = "632196353354629160"
+    MikeyID = "596658100841480192"
+	NenChanID = "228858790265618432"
 )
 
 var MemberNickNames = []string{"Em cũng bị đần", "Chấn bé đù", "Đần không phải tội", "Đần như bot"}
