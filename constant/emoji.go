2c388b8e885f3f627fe309c6a92ea967c979bf1e
Tam Chi Nguyen <nctam109@gmail.com>
2025-03-04 14:13:25 +0700

    Add 3sam rep

--- constant/emoji.go
+++ constant/emoji.go
@@ -1,14 +1,15 @@
 package constant
 
 const (
-	Tsk       = "tsk:1063110732767567994"
-	Aragorn   = "aragorn_megusta:1049991923697467392"
-	TooSad    = "too_sad:894868339246116874"
-	Ree       = "ree:904720707026575391"
-	What      = "what:894868339170615336"
-	SexyPika  = "a:sexypika:869302137279090698"
-	SexyPepe  = "a:pepesexy:840894452338917397"
-	WibuSpank = "a:wibuspank:852781547017994242"
-	Nwng1     = "nung1:948265268428304434"
-	ThietLuon = "02plz:948255285074616380"
+    Tsk       = "tsk:1063110732767567994"
+    Aragorn   = "aragorn_megusta:1049991923697467392"
+    TooSad    = "too_sad:894868339246116874"
+    Ree       = "ree:904720707026575391"
+    What      = "what:894868339170615336"
+    SexyPika  = "a:sexypika:869302137279090698"
+    SexyPepe  = "a:pepesexy:840894452338917397"
+    WibuSpank = "a:wibuspank:852781547017994242"
+    Nwng1     = "nung1:948265268428304434"
+    ThietLuon = "02plz:948255285074616380"
+    Threesome = "a:brse:852973487404220488"
 )
