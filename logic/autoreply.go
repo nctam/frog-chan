95dec5a1f44dff61cae021a40905f2b72e72a394
Vu <35485655+im6h@users.noreply.github.com>
2025-04-08 21:30:35 +0700

    priority fontchan id (#23)

--- logic/autoreply.go
+++ logic/autoreply.go
@@ -69,6 +69,15 @@ func (g *GeneralAutoReply) SendReply(ctx context.Context, s *discord.Session, r 
 		msgToSend.Description = msg.Build()
 	}
 
+	if strings.Contains(r.Content, "sạc") && r.Author.ID == constant.PongChanID {
+		msg.TagUsers = []string{r.Author.ID}
+		msg.Message = constant.MsgReplyTagged[rand.Intn(len(constant.MsgReplyTagged))]
+		msgToSend.Description = msg.Build()
+	} else {
+		log.Warn().Msgf("Reject reply user: %s with %s", r.Author.Username, "sạc")
+        return
+    }
+
 	if msg.HasRef {
 		_, sendMsgErr = s.ChannelMessageSendEmbedReply(r.ChannelID, msgToSend, r.Reference())
 	} else {
