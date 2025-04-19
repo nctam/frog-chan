54f04a0693a6e044e8d367a7288fb97c94178775
Vu <35485655+im6h@users.noreply.github.com>
2025-04-19 10:47:22 +0700

    Update autoreply.go (#24)

--- constant/user.go
+++ constant/user.go
@@ -8,13 +8,15 @@ const (
 )
 
 const (
-	EchChanID = "666563738370834434"
-    CaDuChanID = "376386392311201793"
-    FonChanID = "367309546823221250"
-    PongChanID = "390488049420730369"
-    PiChanID = "632196353354629160"
-    MikeyID = "596658100841480192"
-	NenChanID = "228858790265618432"
+	EchChanID   = "666563738370834434"
+	CaDuChanID  = "376386392311201793"
+	FonChanID   = "367309546823221250"
+	PongChanID  = "390488049420730369"
+	PiChanID    = "632196353354629160"
+	MikeyID     = "596658100841480192"
+	NenChanID   = "228858790265618432"
+	SevenBabyID = "362785897780871168"
+	DucToID     = "592470341037588480"
 )
 
 var MemberNickNames = []string{"Em cũng bị đần", "Chấn bé đù", "Đần không phải tội", "Đần như bot"}
