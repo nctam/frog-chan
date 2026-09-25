bd405b503e7ea4f37832d23fba7b5d3dc5de23a5
Tam Chi Nguyen <nctam@users.noreply.github.com>
2026-09-25 23:35:30 +0700

    feat: Add embedded video reply (#26)

--- logic/autoreply.go
+++ logic/autoreply.go
@@ -1,91 +1,155 @@
 package logic
 
 import (
-	"context"
-	"strings"
+    "context"
+    "fmt"
+    "net/url"
+    "regexp"
+    "strings"
 
-	"golang.org/x/exp/rand"
+    "golang.org/x/exp/rand"
 
-	discord "github.com/bwmarrin/discordgo"
-	"github.com/rs/zerolog"
+    discord "github.com/bwmarrin/discordgo"
+    "github.com/rs/zerolog"
 
-	"kaeru.chan/voz/constant"
-	"kaeru.chan/voz/message"
-	"kaeru.chan/voz/server"
-	"kaeru.chan/voz/utils"
+    "kaeru.chan/voz/constant"
+    "kaeru.chan/voz/message"
+    "kaeru.chan/voz/server"
+    "kaeru.chan/voz/utils"
 )
 
 const (
-	communityLogTag = "GeneralReply"
+    communityLogTag = "GeneralReply"
+)
+
+var (
+    fbRegex = regexp.MustCompile(
+        `https?://(?:www\.)?facebook\.com/(?:watch/?\?v=|share/[rv]/|reel/|[^/]+/videos/|story\.php\?story_fbid=)[a-zA-Z0-9_\-\&\=\?]+`,
+    )
+
+    embedDomain = "facecot.com"
 )
 
 type GeneralAutoReply struct{}
 
 var (
-	config *server.Config
-	_      AutoReply = &GeneralAutoReply{}
+    config *server.Config
+    _      AutoReply = &GeneralAutoReply{}
 )
 
 func init() {
-	config = server.AppConfig
+    config = server.AppConfig
 }
 
 func (g *GeneralAutoReply) SendReply(ctx context.Context, s *discord.Session, r *discord.MessageCreate) {
-	log := zerolog.Ctx(ctx).With().Str(communityLogTag, "SendReply").Logger()
-	if !utils.ShouldReply(ctx, r.Author.ID, config) {
-		log.Warn().Msgf("Reject reply user: %s", r.Author.Username)
-		return
-	}
+    log := zerolog.Ctx(ctx).With().Str(communityLogTag, "SendReply").Logger()
+    if r.Author.ID == s.State.User.ID {
+        return
+    }
 
-	log.Info().Msgf("Accept reply user: %s", r.Author.Username)
-	msg := message.PickMessage(message.DetectMessage(constant.CommunityMessages, r))
+    fixedURL, platform, found := extractURL(r.Content)
+    if found {
+        if sendEmbeddedErr := sendEmbedMessage(s, r.ChannelID, r.Author.ID, platform, fixedURL); sendEmbeddedErr == nil {
+            _ = s.ChannelMessageDelete(r.ChannelID, r.ID)
+            return
+        } else {
+            return
+        }
+    }
 
-	if msg == nil {
-		log.Warn().Msg("Message template not found")
-		return
-	}
+    if !utils.ShouldReply(ctx, r.Author.ID, config) {
+        log.Warn().Msgf("Reject reply user: %s", r.Author.Username)
+        return
+    }
 
-	if taggedUsers := utils.ExtractTaggedUserID(ctx, r.Content, config); taggedUsers != nil {
-		msg.TagUsers = taggedUsers
-	}
+    log.Info().Msgf("Accept reply user: %s", r.Author.Username)
+    msg := message.PickMessage(message.DetectMessage(constant.CommunityMessages, r))
 
-	if msg.ReactEmoji != "" {
-		if err := s.MessageReactionAdd(r.ChannelID, r.ID, msg.ReactEmoji); err != nil {
-			log.Error().Err(err).Msgf("Unable to react message")
-		}
-	}
+    if msg == nil {
+        log.Warn().Msg("Message template not found")
+        return
+    }
 
-	var sendMsgErr error
-	msgToSend := &discord.MessageEmbed{
-		Description: msg.Build(),
-		Image: &discord.MessageEmbedImage{
-			URL: msg.Url,
-		},
-	}
+    if taggedUsers := utils.ExtractTaggedUserID(ctx, r.Content, config); taggedUsers != nil {
+        msg.TagUsers = taggedUsers
+    }
 
-	if strings.Contains(r.Content, "chửi") && msg.Build() == "" {
-		msg.TagUsers = []string{r.Author.ID}
-		msg.Message = constant.MsgReplyTagged[rand.Intn(len(constant.MsgReplyTagged))]
-		msgToSend.Description = msg.Build()
-	}
+    if msg.ReactEmoji != "" {
+        if err := s.MessageReactionAdd(r.ChannelID, r.ID, msg.ReactEmoji); err != nil {
+            log.Error().Err(err).Msgf("Unable to react message")
+        }
+    }
 
-	if strings.Contains(r.Content, "sạc") {
-		if r.Author.ID == constant.PongChanID || r.Author.ID == constant.SevenBabyID || r.Author.ID == constant.DucToID {
-			msg.TagUsers = []string{r.Author.ID}
-			msgToSend.Description = msg.Build()
-		} else {
-			log.Warn().Msgf("Reject reply user: %s with %s", r.Author.Username, "sạc")
-			return
-		}
-	}
+    var sendMsgErr error
+    msgToSend := &discord.MessageEmbed{
+        Description: msg.Build(),
+        Image: &discord.MessageEmbedImage{
+            URL: msg.Url,
+        },
+    }
 
-	if msg.HasRef {
-		_, sendMsgErr = s.ChannelMessageSendEmbedReply(r.ChannelID, msgToSend, r.Reference())
-	} else {
-		_, sendMsgErr = s.ChannelMessageSendEmbed(r.ChannelID, msgToSend)
-	}
+    if strings.Contains(r.Content, "chửi") && msg.Build() == "" {
+        msg.TagUsers = []string{r.Author.ID}
+        msg.Message = constant.MsgReplyTagged[rand.Intn(len(constant.MsgReplyTagged))]
+        msgToSend.Description = msg.Build()
+    }
 
-	if sendMsgErr != nil {
-		log.Error().Err(sendMsgErr).Msg("Unable to reply message")
-	}
+    if strings.Contains(r.Content, "sạc") {
+        if r.Author.ID == constant.PongChanID || r.Author.ID == constant.SevenBabyID || r.Author.ID == constant.DucToID {
+            msg.TagUsers = []string{r.Author.ID}
+            msgToSend.Description = msg.Build()
+        } else {
+            log.Warn().Msgf("Reject reply user: %s with %s", r.Author.Username, "sạc")
+            return
+        }
+    }
+
+    if msg.HasRef {
+        _, sendMsgErr = s.ChannelMessageSendEmbedReply(r.ChannelID, msgToSend, r.Reference())
+    } else {
+        _, sendMsgErr = s.ChannelMessageSendEmbed(r.ChannelID, msgToSend)
+    }
+
+    if sendMsgErr != nil {
+        log.Error().Err(sendMsgErr).Msg("Unable to reply message")
+    }
+}
+
+func extractURL(content string) (string, string, bool) {
+    // Facebook
+    if fbRegex.MatchString(content) {
+        urlStr := fbRegex.FindString(content)
+        u, err := url.Parse(urlStr)
+        if err != nil {
+            return "", "", false
+        }
+
+        if u.Host == "www.facebook.com" {
+            u.Host = "www." + embedDomain
+        } else if u.Host == "facebook.com" {
+            u.Host = embedDomain
+        } else {
+            return "", "", false
+        }
+        return u.String(), "Facebook", true
+    }
+
+    return "", "", false
+}
+
+func sendEmbedMessage(
+    s *discord.Session,
+    channelID string,
+    authorID string,
+    platform string,
+    url string,
+) error {
+    msg := fmt.Sprintf(
+        "\n%s\n\nsauce: <@%s>",
+        url,
+        authorID,
+    )
+
+    _, err := s.ChannelMessageSend(channelID, msg)
+    return err
 }
--- utils/calculator.go
+++ utils/calculator.go
@@ -1,46 +1,56 @@
 package utils
 
 import (
-	"context"
-	"time"
+    "context"
+    "regexp"
+    "time"
 
-	"github.com/rs/zerolog"
-	"golang.org/x/exp/rand"
+    "github.com/rs/zerolog"
+    "golang.org/x/exp/rand"
 
-	"kaeru.chan/voz/server"
+    "kaeru.chan/voz/server"
 )
 
 const (
-	logTag = "Utils"
+    logTag = "Utils"
 )
 
 func makeDecision(probability float64, universal int) (int, bool) {
-	counter := 0
-	for i := 0; i < universal; i++ {
-		rand.Seed(uint64(time.Now().UnixNano()))
-		randValue := rand.Int()
-		if randValue%5 == 0 {
-			counter += 1
-		}
-	}
-	decision := float64(counter)/float64(universal) >= probability
-	return counter, decision
+    counter := 0
+    for i := 0; i < universal; i++ {
+        rand.Seed(uint64(time.Now().UnixNano()))
+        randValue := rand.Int()
+        if randValue%5 == 0 {
+            counter += 1
+        }
+    }
+    decision := float64(counter)/float64(universal) >= probability
+    return counter, decision
 }
 
 func ShouldReply(ctx context.Context, userID string, config *server.Config) bool {
-	logger := zerolog.Ctx(ctx).With().Str(logTag, "ShouldReply").Logger()
-	if StringContains(config.PriorUsers, userID) {
-		logger.Info().Msgf("Prior user: %s found, start reply", userID)
-		return true
-	}
-	counter, decision := makeDecision(config.Probability, config.UniversalSet)
-	logger.Info().Msgf("Total votes: %d ==> Reply decision: %v", counter, decision)
-	return config.Env == "test" || decision
+    logger := zerolog.Ctx(ctx).With().Str(logTag, "ShouldReply").Logger()
+    if StringContains(config.PriorUsers, userID) {
+        logger.Info().Msgf("Prior user: %s found, start reply", userID)
+        return true
+    }
+    counter, decision := makeDecision(config.Probability, config.UniversalSet)
+    logger.Info().Msgf("Total votes: %d ==> Reply decision: %v", counter, decision)
+    return config.Env == "test" || decision
 }
 
 func ShouldRename(ctx context.Context, config *server.Config) bool {
-	logger := zerolog.Ctx(ctx).With().Str(logTag, "ShouldRename").Logger()
-	counter, decision := makeDecision(config.Probability, config.UniversalSet)
-	logger.Info().Msgf("Total votes: %d ==> Rename decision: %v", counter, decision)
-	return config.Env == "test" || decision
+    logger := zerolog.Ctx(ctx).With().Str(logTag, "ShouldRename").Logger()
+    counter, decision := makeDecision(config.Probability, config.UniversalSet)
+    logger.Info().Msgf("Total votes: %d ==> Rename decision: %v", counter, decision)
+    return config.Env == "test" || decision
+}
+
+func extractHttpsLinks(text string) []string {
+    // Pattern looks for 'https://' followed by valid URL characters
+    pattern := `https://[^\s/$.?#].[^\s]*`
+    re := regexp.MustCompile(pattern)
+
+    // FindAllString returns all matches, or nil if none found
+    return re.FindAllString(text, -1)
 }
