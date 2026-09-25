0ecfc3c151ccafc8c0c297ed203ff6852e15b4bc
Tam Chi Nguyen <nctam109@gmail.com>
2026-09-25 23:25:21 +0700

    feat: Add embedded video reply

--- server/config.go
+++ server/config.go
@@ -1,14 +1,15 @@
 package server
 
 type Config struct {
-	BotToken       string   `yaml:"bot-token"`
-	Channels       []string `yaml:"channels"`
-	ExcludedUsers  []string `yaml:"excluded-users"`
-	Probability    float64  `yaml:"probability"`
-	UniversalSet   int      `yaml:"universal-set"`
-	Env            string   `yaml:"env"`
-	MessagePattern string   `yaml:"message-pattern"`
-	PriorUsers     []string `yaml:"prior-users"`
-	Bots           []string `yaml:"bots"`
-	IotioPattern   string   `yaml:"iotio-pattern"`
+    BotToken        string   `yaml:"bot-token"`
+    Channels        []string `yaml:"channels"`
+    ExcludedUsers   []string `yaml:"excluded-users"`
+    Probability     float64  `yaml:"probability"`
+    UniversalSet    int      `yaml:"universal-set"`
+    Env             string   `yaml:"env"`
+    MessagePattern  string   `yaml:"message-pattern"`
+    VideoUrlPattern string   `yaml:"video-url-pattern"`
+    PriorUsers      []string `yaml:"prior-users"`
+    Bots            []string `yaml:"bots"`
+    IotioPattern    string   `yaml:"iotio-pattern"`
 }
